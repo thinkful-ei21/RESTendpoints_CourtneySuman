f35a9071aa14104f100a4e5395b306a31b7feb0c
Suman Bandita <sumanbandita@gmail.com>
2018-05-23 12:17:26 -0400

    v2

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.get('/shopping-list', (req, res) => {
 });
 
 app.get('/recipes', (req, res) => {
-	res.json(Recipes.get());
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
